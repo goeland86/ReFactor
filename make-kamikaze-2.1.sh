cc49e7d40606fdea5c8ff6789eb92e27dbb95f57
Jon C <goeland86@gmail.com>
2018-04-29 23:29:15 +0200

    Enable overlay loading in uEnv.txt

--- make-kamikaze-2.1.sh
+++ make-kamikaze-2.1.sh
@@ -487,6 +487,7 @@ cleanup() {
 prepare_flasher() {
 	cp functions.sh init-eMMC-flasher-v3.sh /opt/scripts/tools/eMMC/
 	sed -i 's/#cmdline=/cmdline=/' /boot/uEnv.txt
+	sed -i 's/#enable_/enable_/' /boot/uEnv.txt
 }
 
 dist() {
