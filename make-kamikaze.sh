5720e6327672991b17b994a36bb758c59c6031ee
Richard Wackerbarth <rkw@dataplex.net>
2018-06-11 08:25:04 -0500

    ••• Have these been done?

--- make-kamikaze.sh
+++ make-kamikaze.sh
@@ -5,25 +5,6 @@ set -e
 exec >  >(tee -ia /root/make-kamikaze.log)
 exec 2> >(tee -ia /root/make-kamikaze.log >&2)
 
-# TODO 2.1:
-# PCA9685 in devicetree
-# Make redeem dependencies built into redeem
-# Remove xcb/X11 dependencies
-# Add sources to clutter packages
-# Slic3r support
-# Edit Cura profiles
-# Remove root access
-# /dev/ttyGS0
-
-# TODO 2.0:
-# After boot,
-# initrd img / depmod-a on new kernel.
-
-# STAGING:
-# Copy uboot files to /boot/uboot
-# Restart commands on install for Redeem and Toggle
-# Update to Clutter 1.26.0+dsfg-1
-
 # Get the versioning information from the entries in version.d/
 export VERSIONING=`pwd`/Packages/version.d
 for f in `ls ${VERSIONING}/*`
