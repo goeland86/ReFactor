0547597c77c2afc2d4804a938a54d0e013e9a0f2
Jon Charnas <goeland86@gmail.com>
2019-11-28 11:39:06 +0100

    Updated dependency order for klipper installation

--- make-kamikaze.sh
+++ make-kamikaze.sh
@@ -5,21 +5,6 @@ set -e
 exec >  >(tee -ia /root/make-kamikaze.log)
 exec 2> >(tee -ia /root/make-kamikaze.log >&2)
 
-# Get the versioning information from the entries in version.d/
-export VERSIONING=`pwd`/Packages/version.d
-for f in `ls ${VERSIONING}/*`
-  do
-    source $f
-  done
-
-echo "**Making ${VERSION}**"
-export LC_ALL=C
-export PATH=`pwd`/Packages:$PATH
-
-install_redeem
-# install_cura
-# install_slic3r
-
-perform_cleanup
+ansible-playbook build_full_klipper_octoprint.yml
 
 echo "Now reboot!"
