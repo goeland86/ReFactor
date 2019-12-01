b59563716a6ef7da3675e39687c0c398da543771
Jon_c <goeland86@gmail.com>
2019-12-01 00:00:42 +0000

    Working linux-preparation playbook

--- make-kamikaze.sh
+++ make-kamikaze.sh
@@ -5,6 +5,6 @@ set -e
 exec >  >(tee -ia /root/make-kamikaze.log)
 exec 2> >(tee -ia /root/make-kamikaze.log >&2)
 
-ansible-playbook build_full_klipper_octoprint.yml
+ansible-playbook ./build_full_klipper_octoprint.yml
 
 echo "Now reboot!"
