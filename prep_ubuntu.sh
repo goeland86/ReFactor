644c4bc0742f54718e8c7917862e36ecba517ce5
Jon_c <goeland86@gmail.com>
2020-03-22 10:59:07 +0000

    FIXED: debian based image properly booting / flashing.

--- prep_ubuntu.sh
+++ prep_ubuntu.sh
@@ -2,7 +2,10 @@
 
 export DEBIAN_FRONTEND=noninteractive 
 echo "Installing ansible"
+
+echo "deb http://deb.debian.org/debian stretch-backports main" >> /etc/apt/sources.list
+
 apt-get update
-apt-get install -y ansible
+apt-get -t stretch-backports install -y ansible git python-pip python3-pip python-virtualenv python3-venv
 
 echo "Now the system is ready to use the ansible playbooks to build images."
