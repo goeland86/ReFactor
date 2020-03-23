3b507831e866e3d027bf5e71f14460403817d3fb
Jon_c <goeland86@gmail.com>
2020-03-23 00:53:01 +0000

    FIXED: Execution of the shell script with user, sudo rights

--- prep_ubuntu.sh
+++ prep_ubuntu.sh
@@ -8,4 +8,6 @@ echo "deb http://deb.debian.org/debian stretch-backports main" >> /etc/apt/sourc
 apt-get update
 apt-get -t stretch-backports install -y ansible git python-pip python3-pip python-virtualenv python3-venv
 
+echo "debian	ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
 echo "Now the system is ready to use the ansible playbooks to build images."
