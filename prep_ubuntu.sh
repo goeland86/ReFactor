29b76a04105ec8be71bdc6431167e660f5f40ffa
Jon_c <goeland86@gmail.com>
2020-04-08 20:22:21 +0100

    FIXED: klipper service dependencies, image size

--- prep_ubuntu.sh
+++ prep_ubuntu.sh
@@ -3,7 +3,10 @@
 export DEBIAN_FRONTEND=noninteractive
 echo "Installing ansible"
 
-apt-get -y remove apache2
+chmod o+r /etc/resolv.conf
+echo "nameserver 1.1.1.1" > /etc/resolv.conf
+
+apt-get -y remove apache2 nginx grub javascript-common libjs* 
 apt-get -y autoremove
 apt-get update
 apt-get -y install ansible git python3-pip python3-venv python-pip python-virtualenv python-apt python3-apt
