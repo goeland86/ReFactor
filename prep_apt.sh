7d590be1cf31ef266c53cd990b44d491a13222ae
Jon_c <goeland86@gmail.com>
2020-05-09 18:09:52 +0100

    FIXED: problem with python2 virtualenv package being available.

--- prep_apt.sh
+++ prep_apt.sh
@@ -6,10 +6,12 @@ echo "Installing ansible"
 chmod o+r /etc/resolv.conf
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 
-apt-get -y remove apache2 nginx grub javascript-common libjs* 
+apt-get -y remove apache2 nginx grub javascript-common libjs*
 apt-get -y autoremove
 apt-get update
-apt-get -y install ansible git python3-pip python3-venv python-pip python-virtualenv python-apt python3-apt
+apt-get -y install ansible git python3-pip python3-venv python-pip python-virtualenv python-apt python3-apt python-setuptools
+pip install --upgrade pip
+pip install --upgrade virtualenv
 
 sed -i 's/gather_timeout=10/gather_timeout=20/g' /etc/ansible/ansible.cfg
 
