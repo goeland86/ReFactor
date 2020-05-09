f13fac8f51219dfe755a5129ad43beb62dc8486a
Jon C <goeland86@gmail.com>
2020-05-09 23:16:06 +0200

    Push changes from master to dev. (#4)
    * Removing unused files (Cura & Slic3r install), renaming and preparing for multi-board build capability
    
    * Fixed some bugs in the recore image creation.
    
    * Fixed call of ansible playbook filename after renaming.
    
    * Bugfixing the recore build. Adding raspbian builds for raspihf and raspi64 arch.
    
    * Added optional selection of system build playbook file.
    
    * Fixed typo in build-image script, added check for the debian user for OctoPrint.
    
    * Fixed missing python2 virtualenv for octoprint.
    
    * Fixed python2 virtualenv package name.
    
    * FIXED: problem with python2 virtualenv package being available.
    
    * Fixing the systemd removal, we want to keep it.
    
    * Fixing the root password setting issue

--- build-image-in-chroot-end-to-end.sh
+++ build-image-in-chroot-end-to-end.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+	echo "We need to know which platform we're building for."
+	echo "Should be one of: {replicape, recore, raspihf, raspi64}"
+	echo "Usage: build-image-in-chroot-end-to-end.sh platform [system-build-script]"
+	exit
+fi
+
+if [ "$#" -eq 2 ]; then
+	SYSTEM_ANSIBLE=$2
+else
+	SYSTEM_ANSIBLE=SYSTEM_klipper_octoprint-DEFAULT.yml
+fi
+
+if [ ! -f ${SYSTEM_ANSIBLE} ]; then
+	echo "Could not find the system build playbook ${SYSTEM_ANSIBLE}. Cannot continue."
+	exit
+fi
+
 set -x
 set -e
 
-for f in `ls Packages/version.d/*`
+
+TARGET_PLATFORM=$1
+
+for f in $(ls BaseLinux/${TARGET_PLATFORM}/*)
   do
     source $f
   done
@@ -10,19 +32,20 @@ if [ -f "customize.sh" ] ; then
   source customize.sh
 fi
 
-TARGETIMAGE=kamikaze-rootfs.img
+TARGETIMAGE=refactor-${TARGET_PLATFORM}-rootfs.img
 MOUNTPOINT=$(mktemp -d /tmp/umikaze-root.XXXXXX)
-UMIKAZE_HOME="/usr/src/Umikaze"
-
-BASEIMAGE=`basename ${BASEIMAGE_URL}`
+REFACTOR_HOME="/usr/src/ReFactor"
 
 if [ ! -f $BASEIMAGE ]; then
-    wget $BASEIMAGE_URL
+    wget $BASEIMAGE_URL -O $BASEIMAGE
 fi
 
 rm -f $TARGETIMAGE
-xz -c -d $BASEIMAGE >> $TARGETIMAGE
+decompress || $(echo "check your Linux platform file is correct!"; exit) # defined in the BaseLinux/{platform}/Linux file
+#if [ $TARGET_PLATFORM == 'replicape' ]; then
 truncate -s 4G $TARGETIMAGE
+#fi
+
 DEVICE=`losetup -P -f --show $TARGETIMAGE`
 
 cat << EOF | fdisk ${DEVICE}
@@ -51,11 +74,11 @@ cp /etc/resolv.conf ${MOUNTPOINT}/etc/resolv.conf
 
 # don't git clone here - if someone did a commit since this script started, Unexpected Things will happen
 # instead, do a deep copy so the image has a git repo as well
-mkdir -p ${MOUNTPOINT}${UMIKAZE_HOME}
+mkdir -p ${MOUNTPOINT}${REFACTOR_HOME}
 
 shopt -s dotglob # include hidden files/directories so we get .git
 shopt -s extglob # allow excluding so we can hide the img files
-cp -r `pwd`/!(*.img*) ${MOUNTPOINT}${UMIKAZE_HOME}
+cp -r `pwd`/!(*.img*) ${MOUNTPOINT}${REFACTOR_HOME}
 shopt -u extglob
 shopt -u dotglob
 
@@ -65,7 +88,14 @@ if [ -f "customize.sh" ]; then
 fi
 
 set +e # allow this to fail - we'll check the return code
-chroot ${MOUNTPOINT} /bin/su -c "cd ${UMIKAZE_HOME} && ./prep_ubuntu.sh && ansible-playbook build_full_klipper_octoprint.yml"
+cat << EOF | chroot ${MOUNTPOINT} su -c passwd
+1234
+kamikaze
+kamikaze
+EOF
+chroot ${MOUNTPOINT} su -c "cd ${REFACTOR_HOME} && ./prep_apt.sh && ansible-playbook ${SYSTEM_ANSIBLE} -T 180"
+
+
 status=$?
 set -e
 
